450f1e8b5a7587d79fd02d9b6e315a05445bbb97
marcusshepp <marcusshepdotcom@gmail.com>
2024-11-04 22:04:30 -0500

    re re resistence

--- src/main.ts
+++ src/main.ts
@@ -4,10 +4,10 @@ canvas.width = 400;
 canvas.height = 400;
 context.fillStyle = "white";
 
-let State = {
+let S = {
 
-    numberOfParticles: 1,
-    particleRadius: 3,
+    numberOfParticles: 1000,
+    particleRadius: 2,
     particles: [],
     theda: 0,
     outerTheda: 0,
@@ -18,80 +18,105 @@ let State = {
 
 };
 
+interface Particle {
+    x: number;
+    y: number;
+    g: number;
+    destY: number;
+    destX: number;
+}
+
 function init(): void {
-    for (let i = 0; i < State.numberOfParticles; i++) {
+    for (let i = 0; i < S.numberOfParticles; i++) {
+        const ranX: number = Math.floor(Math.random() * canvas.width);
+        const ranY: number = Math.floor(Math.random() * canvas.height);
+        const destX: number = Math.floor(Math.random() * canvas.width);
+        const destY: number = Math.floor(Math.random() * canvas.height);
         let particle = {
-            x: 0,
-            y: 0,
-        };
-        State.particles.push(particle);
+            x: ranX,
+            y: ranY,
+            destX: destX,
+            destY: destY,
+            v: 0.5,
+            g: 2,
+        } as Particle;
+        S.particles.push(particle);
     }
 }
 
 function update(): void {
-    /*
-     * The equation of a circle is (x − a)2 + (y − b)2 = r2 
-     * where a and b are the coordinates of the center (a, b)
-     * and r is the radius.
-    * */
 
-    State.theda += 0.02;
-    State.outerTheda += 0.05;
-    for (let i = 0; i < State.particles.length; i++) {
 
-        let a = State.centerParticle.x;
-        let b = State.centerParticle.y;        
-        let r = 50;
-        let x = a + r * Math.cos(State.outerTheda);
-        let y = b + r * Math.sin(State.outerTheda);
-        State.particles[i].x = x;
-        State.particles[i].y = y;
+    for (let i = 0; i < S.particles.length; i++) {
 
+        const p = S.particles[i];
+        if (p.x < p.destX) {
+            S.particles[i].x += S.particles[i].v;
+        }
+        if (p.x > p.destX) {
+            S.particles[i].x -= S.particles[i].v;
+        }
+        if (p.y > p.destY) {
+            S.particles[i].y -= S.particles[i].v;
+        }
+        if (p.y < p.destY) {
+            S.particles[i].y += S.particles[i].v;
+        }
+
+        if (S.particles[i].x > (canvas.width * .98)) {
+            S.particles[i].v *= -1;
+        }
+
+        if (S.particles[i].y > (canvas.height * .98)) {
+            S.particles[i].v *= -1;
+        }
+
+        if (S.particles[i].x < 10) {
+            S.particles[i].v *= -1;
+        }
+
+        if (S.particles[i].y < 10) {
+            S.particles[i].v *= -1;
+        }
+
+        if (S.particles[i].x === S.particles[i].destX &&
+            S.particles[i].y === S.particles[i].destY) {
+
+            const destX: number = Math.floor(Math.random() * canvas.width);
+            const destY: number = Math.floor(Math.random() * canvas.height);
+            S.particles[i].destX = destX;
+            S.particles[i].destY = destY;
+
+        }
+        for (let j = 0; j < S.particles.length; j++) {
+            if (distanceFrom(S.particles[i], S.particles[j]) < 5 &&
+                distanceFrom(S.particles[i], S.particles[j]) > 1) {
+                S.particles[i].v *= -1;
+            } else {
+                S.particles[i].x *= S.velX;
+                S.particles[i].y *= S.velY;
+            }
+        }
     }
 
-    //
-    //let a = canvas.width / 2;
-    //let b = canvas.height / 2;
-    //let r = 50;
-    //let x = a + r * Math.cos(State.theda);
-    //let y = b + r * Math.sin(State.theda);
-    //
-    //State.centerParticle.x = x;
-    //State.centerParticle.y = y;
-
-    State.centerParticle.x += State.velX;
-    State.centerParticle.y += State.velY;
-
-    if (State.centerParticle.x > (canvas.width * .98)) {
-       State.velX = State.velX * -0.1 * Math.PI;
-    }
-
-    if (State.centerParticle.y > (canvas.height * .98)) {
-       State.velY = State.velY * -0.1 * Math.PI; 
-    }
-
-    if (State.centerParticle.x < 10) {
-       State.velX = State.velX * -0.1 * Math.PI; 
-    }
-
-    if (State.centerParticle.y < 10) {
-       State.velY = State.velY * -0.1 * Math.PI; 
-    }
 }
 
 function draw(): void {
 
-    for (let i = 0; i < State.particles.length; i++) {
-        const part = State.particles[i];
+    for (let i = 0; i < S.particles.length; i++) {
+        const part = S.particles[i];
         drawParticle(part);
     }
-    drawParticle(State.centerParticle);
+}
+
+function distanceFrom(part1: Particle, part2: Particle): number {
+   return Math.hypot(part1.x - part2.x, part1.y - part2.y); 
 }
 
 
 function drawParticle(part: {x: number, y: number}) {
     context.beginPath();
-    context.arc(part.x, part.y, State.particleRadius, 0, 2 * Math.PI);
+    context.arc(part.x, part.y, S.particleRadius, 0, 2 * Math.PI);
     context.closePath();
     context.fill();
 }
